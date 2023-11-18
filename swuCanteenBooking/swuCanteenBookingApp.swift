cf3afbf1d0f48f0739706af473f13fa6a7764de2
Phongsathon Makdankang <phongsathon.mdk@gmail.com>
2023-11-18 20:13:34 +0700

    Initial Commit

---
+++ swuCanteenBooking/swuCanteenBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swuCanteenBookingApp.swift
+//  swuCanteenBooking
+//
+//  Created by xqst on 18/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct swuCanteenBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
